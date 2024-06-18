5d40224cceefdf23aad59bf1f21431b2d7063509
hiteshprajapati <hiteshkumar.prajapati@gmail.com>
2024-06-18 20:34:11 +0000

    library transaction

---
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2024, Hitesh Prajapati and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
+
+
+class LibraryTransaction(Document):
+    
+	def before_submit(self):
+		if self.type == "Issue":
+			self.validate_issue()
+            # set the article status to be Issued 
+			article = frappe.get_doc("Article", self.article)
+			article.status = "Issued"
+			article.save()
+		
+		elif self.type == "Return":
+			self.validate_return()
+            # set the article status to be Available
+			article = frappe.get_doc("Article", self.article)
+			article.status = "Available"
+			article.save()
+	
+	def validate_issue(self):
+		self.validate_membership()
+		article = frappe.get_doc("Article", self.article)
+        # article cannot be issued if it is already issued
+		if article.status == "Issued":
+			frappe.throw("Article is already issued by another member")
+	
+	def validate_return(self):
+		article = frappe.get_doc("Article", self.article)
+        # article cannot be returned if it is not issued first
+		if article.status == "Available":
+			frappe.throw("Article cannot be returned without being issued first")
+
+	#  check if a valid membership exist for this library member
+	def validate_membership(self):
+		valid_membership = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                "from_date": ("<", self.date),
+                "to_date": (">", self.date),
+            },
+		)
+		if not valid_membership:
+			frappe.throw("The member does not have a valid membership")
---
+++ library_management/library_management/doctype/library_transaction/test_library_transaction.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hitesh Prajapati and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryTransaction(FrappeTestCase):
+	pass
